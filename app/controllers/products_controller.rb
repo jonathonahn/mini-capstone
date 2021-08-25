551ec970879a2ad84eaf031482e23d701ae1156f
jonathon <ahnjon77@gmail.com>
2021-08-25 10:35:40 -0700

    adds controller methods to call single products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,18 @@
 class ProductsController < ApplicationController
+
   def all_products
     product = Product.all
     render json: product
   end
+
+  def first_product
+    product = Product.first 
+    render json: product
+  end
+
+  def philodendron
+    product = Product.find_by(name: "Philodendron")
+    render json: product
+  end
+
 end
