79f013659cfd1a7375a08910e93dcd57c06803b8
jonathon <ahnjon77@gmail.com>
2021-09-14 10:33:59 -0700

    adds categories of products, creates join table to associate categories and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :supplier
+  
   has_many :images
   has_many :orders
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {greater_than: 0 }
   validates :description, length: { in: 10..500 }
